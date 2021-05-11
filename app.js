844dab3661e5f4a43c72de65f5c8ee663600a1c0
afwyou <afwyousenge@gmail.com>
2021-05-11 16:09:19 +0800

    setup hbs bodyparser methodoverride and listen in app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const express = require('express')
 const app = express()
 require('./config/mongoose')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser, urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+
+
+app.listen(3000, () => {
+  console.log('Listen to the http://localhost:3000')
+})
