694fdf7a20acbbe5d0b8ddc98caaa475a2330214
afwyou <afwyousenge@gmail.com>
2021-05-12 17:00:37 +0800

    set categorySeeder

---
+++ models/Seeder/categorySeeder.js
@@ -0,0 +1,18 @@
+const models = require('../category')
+const db = require('../../config/mongoose')
+const categoryList = require('./categoryList.json').category
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+  for (let i = 0; i < categoryList.length; i++) {
+    models.create({
+      category: categoryList[i].name,
+      categoryIcon: categoryList[i].icon,
+    })
+  }
+
+  console.log('done!')
+
+
+})
