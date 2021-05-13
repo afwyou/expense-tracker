24f94f48fde7ca610f60040f243f4f62e2abb544
afwyou <afwyousenge@gmail.com>
2021-05-13 10:49:04 +0800

    set home.js and app.js about home page
    Routes about records haven't completed, so if i write in to index.js, app.js can't work , because router in records.js is empty, and there is no router to export for index.js.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 require('./config/mongoose')
-const exphbs = require('express-handlebars')
+
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const routes = require('./routes/index')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use((bodyParser.urlencoded({ extended: true })))
 app.use(methodOverride('_method'))
-
+app.use(routes)
 
 
 app.listen(3000, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,4 +1,39 @@
 const express = require('express')
 const router = express.Router()
 //require model
-const Record = require('')
+const Record = require('../../models/record')
+
+
+//定義路由首頁面
+router.get('/', (req, res) => {
+  const amount = Record.aggregate([
+    {
+      $group: {
+        _id: null,
+        amount: { $sum: "$amount" }
+      }
+    }
+  ]).exec()
+  const record = Record.aggregate([
+    {
+      $project: {
+        name: 1,
+        category: 1,
+        amount: 1,
+        merchant: 1,
+        date: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+        categoryIcon: 1,
+      }
+    }
+  ]).exec()
+
+  Promise.all([amount, record])
+    .then(([amount, record]) => {
+      const totalamount = amount[0]
+      res.render('index', { totalamount, record })
+    })
+    .catch(error => console.error(error))
+})
+
+
+module.exports = router
