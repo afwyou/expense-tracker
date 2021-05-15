7c0d07985745a0125c93e96049f569d7d1002c42
afwyou <afwyousenge@gmail.com>
2021-05-16 07:19:07 +0800

    add edit.hbs and route in records.js

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -20,5 +20,23 @@ router.post('/', (req, res) => {
     .then(() => res.redirect('/'))
     .catch((error) => console.log(error))
 })
+// 修改
+// 每一個按鈕上有綁定個別id的路由連結
+//此路由器的設定在id這個欄位已設定為變數
+//如此可以透過req.params取得之後，去mongodb找到對應的資料
+router.get('/:id/edit', (req, res) => {
+  //取得名為id的路由變數
+  const id = req.params.id
+  const categoryList = []
+  Category.find()
+    .lean()
+    .then((items) => {
+      items.forEach((item) => categoryList.push(item.category))
+    })
+
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record, categoryList }))
+})
 
 module.exports = router
