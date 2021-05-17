ec60609aef6b0d7b41c6d065cbe2f3881406cc3c
afwyou <afwyousenge@gmail.com>
2021-05-17 16:00:54 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+const PORT = process.env.PORT || 3000
 require('./config/mongoose')
 
 const bodyParser = require('body-parser')
@@ -22,6 +23,6 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 
-app.listen(3000, () => {
-  console.log('Listen to the http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`Listen to the http://localhost:${PORT}`)
 })
